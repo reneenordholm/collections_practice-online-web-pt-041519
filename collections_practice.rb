0eb2f535e3ecfd7fb4dbab6ffbf31311f47e0652
Renee Nordholm <reneenordholm@gmail.com>
2019-04-29 00:41:03 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by { |word| word.length } 
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each { |name| name[2] = "$" }
+ end
+ 
+def find_a(array)
+  array.find_all { |word| word.start_with?("a") }
+end
+
+def sum_array(array)
+  array.inject { |sum, n| sum + n }
+end
+
+def add_s(array)
+  array.collect { |word| array[1] == word ? word : word + "s" }
+end
